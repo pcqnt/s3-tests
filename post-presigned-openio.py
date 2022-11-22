680ddb77687111b62e6747422b77d2e6b192d1a8
Olivier Picquenot <pcqnt@users.noreply.github.com>
2022-11-22 17:08:11 +0100

    Update post-presigned-openio.py

--- post-presigned-openio.py
+++ post-presigned-openio.py
@@ -2,16 +2,7 @@ import logging
 import boto3
 from botocore.exceptions import ClientError
 import os
-ACCESS_KEY = os.environ['ACCESS_KEY']
-SECRET_KEY = os.environ['SECRET_KEY']
-REGION_NAME = "gra"
-BUCKET_NAME = "bucket-standard-gra-dd"
-ENDPOINT_URL="https://s3.gra.io.cloud.ovh.net"
-OBJECT_KEY="upload.txt"
-session = boto3.Session(aws_access_key_id=ACCESS_KEY,
-              aws_secret_access_key=SECRET_KEY,
-              region_name=REGION_NAME)
-s3_client = session.client(service_name='s3', endpoint_url=ENDPOINT_URL)
+import requests    # To install: pip install requests
 
 def create_presigned_post(bucket_name, object_name,
                           fields=None, conditions=None, expiration=3600):
@@ -29,7 +20,7 @@ def create_presigned_post(bucket_name, object_name,
     """
 
     # Generate a presigned S3 POST URL
-    s3_client = boto3.client(service_name='s3', endpoint_url="https://s3.gra.io.cloud.ovh.net")
+    s3_client = session.client(service_name='s3', endpoint_url="https://s3.gra.io.cloud.ovh.net")
     try:
         response = s3_client.generate_presigned_post(bucket_name,
                                                      object_name,
@@ -43,18 +34,31 @@ def create_presigned_post(bucket_name, object_name,
     # The response contains the presigned URL and required fields
     return response
 
-import requests    # To install: pip install requests
+def main():
+    ACCESS_KEY = os.environ['ACCESS_KEY']
+    SECRET_KEY = os.environ['SECRET_KEY']
+    REGION_NAME = "gra"
+    BUCKET_NAME = "opi-standard-gra"
+    ENDPOINT_URL="https://s3.gra.io.cloud.ovh.net"
+    OBJECT_KEY="upload.txt"
+    session = boto3.Session(aws_access_key_id=ACCESS_KEY,
+                aws_secret_access_key=SECRET_KEY,
+                region_name=REGION_NAME)
 
-# Generate a presigned S3 POST URL
-object_name = 'upload.txt'
-response = create_presigned_post(BUCKET_NAME, object_name)
-if response is None:
-    exit(1)
 
-# Demonstrate how another Python program can use the presigned URL to upload a file
-with open(object_name, 'rb') as f:
-    files = {'file': (object_name, f)}
-    http_response = requests.post(response['url'], data=response['fields'], files=files)
-# If successful, returns HTTP status code 204
-logging.info(f'File upload HTTP status code: {http_response.status_code}')
+    # Generate a presigned S3 POST URL
+    object_name = 'upload.txt'
+    response = create_presigned_post(BUCKET_NAME, object_name)
+    if response is None:
+        exit(1)
+
+    # Demonstrate how another Python program can use the presigned URL to upload a file
+    with open(object_name, 'rb') as f:
+        files = {'file': (object_name, f)}
+        http_response = requests.post(response['url'], data=response['fields'], files=files)
+    # If successful, returns HTTP status code 204
+    logging.info(f'File upload HTTP status code: {http_response.status_code}')
+
+if __name__ == main :
+    main()
  
